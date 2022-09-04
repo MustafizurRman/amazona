41fa056feaf5bcbe2fb3530c96f42990dd040dfb
MustafizurRman <sheikh.mustafizur.rahman@g.bracu.ac.bd>
2022-09-04 15:19:08 +0600

    Manag_State_By_Reducer_Hook

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route exact path="/product/:slug" element={<ProductScreen />} />
+            <Route exact path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+export default App;
