44882e38b7f9277be4ec538e0f281413f5f2a5df
MustafizurRman <sheikh.mustafizur.rahman@g.bracu.ac.bd>
2022-09-06 04:26:21 +0600

    added-rating-react-bootstrap-price

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
-//hello
+//hello wiw
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
