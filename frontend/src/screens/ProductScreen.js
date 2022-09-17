a86e77273cd8bbaff23c562d250fc35fe18aaac3
MustafizurRman <sheikh.mustafizur.rahman@g.bracu.ac.bd>
2022-09-17 06:12:52 +0600

    CartScreen_completed

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -52,6 +52,7 @@ function ProductScreen() {
   }, [slug]);
   const { state, dispatch: contextDispatch } = useContext(Store);
   const { cart } = state;
+
   const addToCartHandler = async () => {
     const existItem = cart.cartItems.find((x) => x._id === product._id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
